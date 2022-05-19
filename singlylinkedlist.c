07accdac5b73ee286cc0fda43ee445e43d322713
Steven D'Addario <Justaservent@hotmail.com>
2022-05-19 01:38:48 -0500

    Added a delete_function as a second 'optional' argument to create_singly_linked_list.
    delete_function will be called when the value is delete allow the freeing of members of structs etc that are on the heap that belong to the type value being
    stored in the linked list.
    
    Also some bug fixes

--- singlylinkedlist.c
+++ singlylinkedlist.c
@@ -1,5 +1,6 @@
 #include <stdlib.h> 
 #include <stdio.h>
+#include <string.h> 
 
 typedef struct singly_linked_list_node {
     struct singly_linked_list_node *next; 
@@ -12,10 +13,28 @@ typedef struct singly_linked_list {
     singly_linked_list_node *tail; 
     unsigned long length; 
     void (*add_function)(singly_linked_list_node *node, void *value); 
+    void (*delete_function)(singly_linked_list_node *node); 
 } singly_linked_list; 
 
 
-singly_linked_list *create_singly_linked_list(void (*f)(singly_linked_list_node *node, void *value)){
+// singly_linked_list will take two functions arguments. The 2nd can be NULL if our type 
+
+// void add_function(singly_linked_list_node *node, void value*)
+//      takes node pointer and value void pointer for the node value. 
+//      What we need to do is set the node value to the value of the void pointer
+//      to do this we need to cast to the correct type for the void pointer 
+//      if the type is a struct with members on the heap we need to 
+//          1. create location on the heap and copy the values from those members. 
+//          2. Set the pointer members to the address of these new locations
+
+// void delete_function(singly_linked_list_node * node)
+//      we pass when our value type has members on the heap that need to be freed. 
+//      When our type does not have members on the heap we pass NULL
+//      Inside we: 
+//          1. we pass node->value->member_name to free() for each member that is on the heap
+//          2. pass node-value to free() at the end of the function.
+
+singly_linked_list *create_singly_linked_list(void (*f)(singly_linked_list_node *node, void *value), void (*g)(singly_linked_list_node * node)){
     singly_linked_list *list = malloc(sizeof(singly_linked_list));
     if(list == NULL){
         printf("\nError\n");
@@ -25,6 +44,7 @@ singly_linked_list *create_singly_linked_list(void (*f)(singly_linked_list_node 
     list->tail = NULL; 
     list->length = 0; 
     list->add_function = f; 
+    list->delete_function = g; 
     return list; 
 }
 
@@ -49,12 +69,17 @@ unsigned long singly_linked_list_push(singly_linked_list *list, void *item){
 }
 
 unsigned long singly_linked_list_pop(singly_linked_list *list){
-    if(list->length = 0){
+    if(list->length == 0){
         return 0; 
     }
     
     if(list->head == list->tail){
-        free(list->head->value);
+        if(list->delete_function != NULL){
+            list->delete_function(list->head);
+        }
+        else{
+            free(list->head->value);
+        }
         free(list->head);
         list->head = NULL; 
         list->tail = NULL;
@@ -65,7 +90,12 @@ unsigned long singly_linked_list_pop(singly_linked_list *list){
             cur = cur->next; 
         }
 
-        free(cur->next->value); 
+        if(list->delete_function != NULL){
+            list->delete_function(cur->next); 
+        }
+        else{
+            free(cur->next->value); 
+        }
         free(cur->next); 
         cur->next = NULL; 
         list->tail = cur; 
@@ -95,6 +125,11 @@ void *singly_linked_list_get(singly_linked_list *list, unsigned long index){
 
 
 void destroy_singly_linked_list(singly_linked_list *list){
+    if(list->length == 0){
+        free(list); 
+        return;
+    }
+
     singly_linked_list_node *collection[list->length]; 
 
     singly_linked_list_node *cur = list->head; 
@@ -106,16 +141,32 @@ void destroy_singly_linked_list(singly_linked_list *list){
 
     for(unsigned long i = list->length - 1; i > 0; i--){
         cur = collection[i]; 
-        free(cur->value); 
+        if(list->delete_function != NULL){
+            list->delete_function(cur);
+        }
+        else{
+            free(cur->value); 
+        }
         free(cur->next); 
     }
 
     cur = collection[0];
     if(cur != NULL){
         free(cur->next); 
-        free(cur->value); 
+        if(list->delete_function != NULL){
+            list->delete_function(cur);
+        }
+        else{
+            free(cur->value); 
+        }
         free(cur); 
     }
 
     free(list); 
 }
+
+typedef struct car {
+    char *color; 
+    float weight; 
+} car;
+
